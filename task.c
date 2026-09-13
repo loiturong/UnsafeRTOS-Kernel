bd832bbe0b6bcba5a8966ca4796f936107e61973
loiturong <loitruong2345@gmail.com>
2026-09-13 16:35:21 +0700

    fix: stack alligned operation
    Previously, just modify the data in stack address instead of modify
    it...

--- task.c
+++ task.c
@@ -56,8 +56,10 @@ void task_create_static(
 /* -------- Function: Static Implementation     -------- */
 uintptr_t *task__init_context(uintptr_t *st, uintptr_t *p_task_entry)
 {
-	// Align by 8-byte;
-	*st &= ~(0x07);
+	// Align by 8-bytes
+	uintptr_t alligned_st = (uintptr_t)st;
+	alligned_st &= ~(0x07);
+	st = (uintptr_t *)alligned_st;
 
 	*(--st) = 0x01000000;			// xPRS
 	*(--st) = (uintptr_t)p_task_entry;	// PC
